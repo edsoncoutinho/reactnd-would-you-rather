eac75da5d2c85d3f32fee2a1a94b1a0c1c6fc580
Edson Coutinho <edson.coutinho@kroton.com.br>
2019-05-31 16:07:40 -0300

    feat: Starting project with create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { getInitialData } from '../utils/api';
+
+function App() {
+  console.log(getInitialData);
+  return (
+    <div>
+      Starter code.
+    </div>
+  );
+}
+
+export default App;
